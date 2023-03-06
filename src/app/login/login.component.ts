4c7c4f7e49f7458dd3c47517174046851e7f0c9c
SJ <sarin.pv@terex.com>
2023-03-06 12:17:29 +0530

    bank1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,35 +18,56 @@ export class LoginComponent {
     1003:{username:"manu",acno:1003,password:"abc123",balance:0}
   }
   
-  login(){
-    var acnum=this.acno
-    var psw=this.psw
-    var userDetails=this.userDetails
+//   login(){
+//     var acnum=this.acno
+//     var psw=this.psw
+//     var userDetails=this.userDetails
 
-    if(acnum in userDetails){
-      if(psw==userDetails[acnum]["password"]){
-        alert("login success")
-      }
-      else{
-        alert("incorrect password")
-      }
-  }
-  else{
-    alert("incorrect acno")
-  }
-}
-  acnoChange(event:any){
-    // console.log(event.target.value);
-    this.acno=event.target.value
-    // console.log(this.acno);
+//     if(acnum in userDetails){
+//       if(psw==userDetails[acnum]["password"]){
+//         alert("login success")
+//       }
+//       else{
+//         alert("incorrect password")
+//       }
+//   }
+//   else{
+//     alert("incorrect acno")
+//   }
+// }
+//   acnoChange(event:any){
+//     // console.log(event.target.value);
+//     this.acno=event.target.value
+//     // console.log(this.acno);
     
 
-  }
-  passChange(event:any){
-    // console.log(event);
-    this.psw=event.target.value
-    // console.log(this.psw);
+//   }
+//   passChange(event:any){
+//     // console.log(event);
+//     this.psw=event.target.value
+//     // console.log(this.psw);
     
     
+//   }
+
+
+
+
+login(acnum:any,psw:any){
+      var acnum=acnum.value
+      var psw=psw.value
+      var userDetails=this.userDetails
+  
+      if(acnum in userDetails){
+        if(psw==userDetails[acnum]["password"]){
+          alert("login success")
+        }
+        else{
+          alert("incorrect password")
+        }
+    }
+    else{
+      alert("incorrect acno")
+    }
   }
 }
